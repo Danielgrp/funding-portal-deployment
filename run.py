9bb8b6166aa20bca63fc72606d5b5d1cfe407766
Dan Llew <daniel@globalresearchpartnerships.com>
2025-06-14 16:32:58 +0100

    Moved requirements.txt, run.py, runtime.txt to root for Render

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ from app import create_app
 
 app = create_app()
 
-<<<<<<< HEAD
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
-=======
->>>>>>> ff9a10e (Flattened project structure for Render deployment)
 
